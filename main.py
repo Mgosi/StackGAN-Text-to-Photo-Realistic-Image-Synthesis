afd16e9d5610a3d0631b642f0046a8803e2d413a
Mgosi <mahigosi@gmail.com>
2019-11-28 13:59:01 -0500

    Update Main

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from training import Trainer
-from data_preprocessing import load_dataset
+#from training import Trainer
+from data_preprocessing import BirdDataset
 import numpy as np
+from initValues import config
+import torch.utils.data as utils
 
 
-# X_train, y_train, embd_train = load_dataset(f_path='../birds/train/filenames.pickle',
-#                                             cid_path='../birds/train/class_info.pickle',
-#                                             direc='../birds/CUB_200_2011/CUB_200_2011/',
-#                                             emb_path='../birds/train/char-CNN-RNN-embeddings.pickle',
-#                                             imgSize=(64,64))
+dataset = BirdDataset(filePath='./birds/train/filenames.pickle',
+                                            cidPath='./birds/train/class_info.pickle',
+                                            dataDir='./birds/CUB_200_2011/CUB_200_2011/',
+                                            embPath='./birds/train/char-CNN-RNN-embeddings.pickle',
+                                            imgSize=(64,64))
+                                            
+                                        
+                                            
+                                            
+                                            #batchSize = config.TRAIN.BATCH_SIZE)
+#print(trainDataset)
 
-
+dataLoader = utils.DataLoader(
+    dataset, batch_size=config.TRAIN.BATCH_SIZE, shuffle=True, num_workers=2
+)
